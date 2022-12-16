200d275110dca926edc91a62d01c622db84a5f45
meeav <105487966+meeav@users.noreply.github.com>
2022-12-16 10:14:23 +0100

    Update tag.ps1

--- tag.ps1
+++ tag.ps1
@@ -1,55 +1,26 @@
 Write-Host "testing tag"
 
-function New-Tag {
-    [CmdletBinding(SupportsShouldProcess=$true)]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string] $Tag)
-    process {
-        if ($PSCmdlet.ShouldProcess("git tag $Tag")) {
-            Write-Verbose "git tag $Tag"
-            git tag $Tag
+Function TagSemantic ($tagPrefix) {
+    $commitMessage = [System.String] (git show -s --format=%s (git rev-parse HEAD)).ToLower()
+    $currentTags = @(git tag --list "$($tagPrefix)*")
+    Write-Host $commitMessage
+    Write-Host $currentTags
+    if ($currentTags.Count -eq 0) {
+        $tag = "$($tagPrefix)_v0.0.1"
+    }
+    else {
+        $currentVersions = $currentTags | ForEach-Object { [System.Version]$_.Split('_v')[1] } | Sort-Object -Descending
+        $lastVersion = $currentVersions[0]
+        if ($commitMessage.Contains("major")) {
+            $tag = "$($tagPrefix)_v$($lastVersion.Major+1).0.0"
+        }
+        elseif ($commitMessage.Contains("minor")) {
+            $tag = "$($tagPrefix)_v$($lastVersion.Major).$($lastVersion.Minor+1).0"
+        }
+        else {
+            $tag = "$($tagPrefix)_v$($lastVersion.Major).$($lastVersion.Minor).$($lastVersion.Build+1)"
         }
     }
-}
-
-function New-SemverTag {
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    param(
-        [Parameter(ParameterSetName = "BreakingChange")]
-        [switch] $BreakingChange = $false,
-        [Parameter(ParameterSetName = "NewFeature")]
-        [switch] $NewFeature = $false,
-        [Parameter(ParameterSetName = "BugFix")]
-        [switch] $BugFix = $false)
-
-    $v = Get-Version
-    [int] $Major = $v.Major;
-    [int] $Minor = $v.Minor;
-    [int] $Build = $v.Build;
-
-    $oldTag = 'v{0}.{1}.{2}' -f $Major, $Minor, $Build
-
-    if ($BreakingChange) {
-        $Major += 1;
-        $Minor = 0;
-        $Build = 0;
-    }
-
-    if ($NewFeature) {
-        $Minor += 1;
-        $Build = 0;
-    }
-
-    if ($BugFix) {
-        $Build += 1;
-    }
-
-    $newTag = 'v{0}.{1}.{2}' -f $Major, $Minor, $Build
-    $message = "Moving from $oldTag to $newTag"
-
-    if ($PsCmdlet.ShouldProcess($message)) {
-        Write-Host $message
-        New-Tag $newTag
-    }
+    Write-Host "Applying semantic tag $tag"
+    git tag $tag -m "auto-generated tag"
 }
